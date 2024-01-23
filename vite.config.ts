7129cc426bd121f293576eb4e7980200f859c855
alekseibaryshnikov <a250188@yandex.ru>
2024-01-23 23:14:16 +0100

    Changed pipeline and vite cfg

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import { VitePWA } from 'vite-plugin-pwa';
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
+  base: '/<repository>/', // for GitHub Pages
   plugins: [
     react(),
     VitePWA({
