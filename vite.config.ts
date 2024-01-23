02eb545bfc39dc724b53197ee1d14b6b4882ff20
alekseibaryshnikov <a250188@yandex.ru>
2024-01-23 23:27:12 +0100

    Vite cfg sub-domain

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import { VitePWA } from 'vite-plugin-pwa';
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  base: '/<repository>/', // for GitHub Pages
+  base: '/lp-tsn/', // for GitHub Pages
   plugins: [
     react(),
     VitePWA({
