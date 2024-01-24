6099caa615039aadf2693904d4aaccd90c9a6986
alekseibaryshnikov <33921918+alekseibaryshnikov@users.noreply.github.com>
2024-01-24 00:25:09 +0000

    Deploying to gh-pages from @ alekseibaryshnikov/lp-tsn@34ef0f1fb47c80255bf0c4613cc155e9761d4a3f 🚀

---
+++ assets/allPathsLoader-2r9cBnsw.js
@@ -0,0 +1,7 @@
+import{_ as o,a as n,b as i}from"./index-NYtCwG2V.js";var _=function(e,a){return o(void 0,void 0,void 0,function(){var t;return n(this,function(r){switch(r.label){case 0:return[4,i(()=>import("./allPaths-a1YO08qE.js"),__vite__mapDeps([0,1,2,3,4]))];case 1:return t=r.sent().getIconPaths,[2,t(e,a)]}})})};export{_ as allPathsLoader};
+function __vite__mapDeps(indexes) {
+  if (!__vite__mapDeps.viteFileDeps) {
+    __vite__mapDeps.viteFileDeps = ["assets/allPaths-a1YO08qE.js","assets/index-7lLlfVks.js","assets/index-KsG0Chs-.js","assets/index-NYtCwG2V.js","assets/index-sgTzN7Ge.css"]
+  }
+  return indexes.map((i) => __vite__mapDeps.viteFileDeps[i])
+}
